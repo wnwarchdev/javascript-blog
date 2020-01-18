095c964201d79db234fec8a6e2af34b506c197cc
Wojtek Nowak <billnewmann@gmail.com>
2020-01-18 18:28:02 +0000

    Relocated previous code; added code that adds class to clicked eleemnt

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
 const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
   /* add class 'active' to the clicked link */
+
+  clickedElement.classList.add("active");
+
+
+console.log('clickedElement:', clickedElement);
+//console.log('clickedElement (with plus): ' + clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
 
-
-
-  /* remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
 
