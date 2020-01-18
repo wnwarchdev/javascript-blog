5c594cf0d2b38f8ef240daad9ba19faf83e3c577
Wojtek Nowak <billnewmann@gmail.com>
2020-01-18 17:59:35 +0000

    Added loops to remove class from links and articles

--- js/script.js
+++ js/script.js
@@ -3,8 +3,19 @@ const titleClickHandler = function(event){
   console.log(event);
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
   /* add class 'active' to the clicked link */
+  const activeArticles = document.querySelectorAll('.post');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
+
+
 
   /* remove class 'active' from all articles */
 
