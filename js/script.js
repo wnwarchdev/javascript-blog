980b74416be36f69ec3ba949bd779b0a98d90a63
Wojtek Nowak <billnewmann@gmail.com>
2020-01-19 17:21:25 +0000

    Add generateTitleLinks; Modified titleClickHandler

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+{
 const titleClickHandler = function(event){
   event.preventDefault();
   const clickedElement = this;
@@ -39,8 +40,75 @@ const titleClickHandler = function(event){
 
 }
 
+/*
 const links = document.querySelectorAll('.titles a');
+console.log("links here: " , links); // reveals links
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+}
+*/
+
+// }{  break in functions removed
+
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+//document.addEventListener('DOMContentLoaded', generateTitleLinks());
+
+function generateTitleLinks () {
+  console.log("start!"); //check if function starts
+
+  let titleList = document.querySelector(optTitleListSelector); //global constans
+  console.log(titleList);
+
+/* [DONE] remove left column innerHTML */
+
+//const removedLeft=document.getElementsByClassName("sidebar").innerHTML="";
+  document.querySelector(optTitleListSelector).innerHTML="";
+  console.log("left links removed");
+
+/* [DONE] for each article: add article ID to const; get article title and add it to const; create html link code with the two consts; add link to left column */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log(articles , " :list of articles");
+
+  let html = ''; //as per example ; will not work inside loop
+
+  for (let article of articles) {
+  console.log(article);
+
+  const articleId = article.getAttribute("id");
+  console.log("ID is: " + articleId);
+
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+  console.log("title is: " + articleTitle);
+
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+  console.log("html link is: " + linkHTML);
+
+  //let titleList = document.querySelector(optTitleListSelector).insertAdjacentHTML( "afterbegin", linkHTML );
+
+  //titleList.insertAdjacentHTML('afterbegin',linkHTML ); // titleList = (...) nie działa
+  //console.log(titleList);
+
+   //console.log("before addition", html);
+   html = html + linkHTML;
+   //console.log("after addition: ", html);
+}
+//console.log("html after loop: " +  html);
+titleList.innerHTML = html;
+
+//moved from titleClickHandler()
+const links = document.querySelectorAll('.titles a');
+console.log("links here: " , links); // reveals links
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
+
+}
+
+generateTitleLinks()
 }
